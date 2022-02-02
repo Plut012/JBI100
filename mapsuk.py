7be0a8e86f6933012f5c3946eca072111209e409
Cecilia Liu <74497790+Cecilia-Liu@users.noreply.github.com>
2022-02-02 20:29:45 +0100

    Update mapsuk.py

--- mapsuk.py
+++ mapsuk.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, redirect, url_for, request
+from datetime import datetime, timedelta
+import time
+import json
+import os
+from trending import get_trending
+
+app = Flask(__name__)
+
+@app.route('/')
+
+if __name__ == "__main__":
+     app.debug = False
+     port = int(os.environ.get('PORT', 33507))
+     waitress.serve(app, port=port)
+
 
 import dash
 import dash_core_components as dcc
